a0efaaf03d81d2cba85ee8be52059393917b6b0d
Sylver Imhoff <Sylver-John.IMHOFF.ext@boursorama.fr>
2017-02-16 14:14:16 +0100

    operateur

--- base/main.go
+++ base/main.go
@@ -4,6 +4,8 @@ import(
 	"fmt"
 )
 
+const constante string = "Constante"
+
 func main() {
 	var toto string
 	toto = "hello"
@@ -29,6 +31,46 @@ func main() {
 
 	b:= B{a, "from france"}
 	fmt.Println(b.A.Hello, b.A.World, b.From)
+
+	var x, y int
+	x = 1
+	y = 2
+	fmt.Println(x + y)
+
+	/*
+		Les opérateurs de bases :
+		+    &     +=    &=     &&    ==    !=    (    )
+		-    |     -=    |=     ||    <     <=    [    ]
+		*    ^     *=    ^=     <-    >     >=    {    }
+		/    <<    /=    <<=    ++    =     :=    ,    ;
+		%    >>    %=    >>=    --    !     ...   .    :
+		&^          &^=
+	*/
+
+	fmt.Println(constante)
+
+    for compteur := 0; compteur < 5; compteur++ {
+        fmt.Println(compteur)
+    }
+
+    if 1 != 2 {
+    	fmt.Println("1 != 2")
+    } else if 1 == 3 {
+    	fmt.Println("on a un problème")
+    } else {
+    	fmt.Println("on a définitivement un problème")
+    }
+
+    switchTo := 1
+    switch switchTo {
+    case 1:
+    	fmt.Println(switchTo)
+    case 2:
+    	fmt.Println(switchTo)
+	default:
+		fmt.Println(switchTo)
+   	}
+
 
 }
 
