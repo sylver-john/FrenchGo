13939c75eec620abc6e9f737eb1e9d9a7c89d58e
Sylver Imhoff <Sylver-John.IMHOFF.ext@boursorama.fr>
2017-02-17 12:10:36 +0100

    sql

--- confirmé/http/src/handler/handler.go
+++ confirmé/http/src/handler/handler.go
@@ -3,9 +3,11 @@ package handler
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"./services"
 )
 
 
 func HandlerRequest(c *gin.Context) {
+	services.doInsert()
   	c.JSON(http.StatusOK, "hello world!")
 }
---
+++ confirmé/http/src/services/sqlManager.go
@@ -0,0 +1,5 @@
+package services
+
+func doInsert() {
+	// do insert in sql table
+}
