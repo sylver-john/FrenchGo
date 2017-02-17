650fae3a7cb40738ad49b7faefa8c3085a2b8af6
Sylver Imhoff <Sylver-John.IMHOFF.ext@boursorama.fr>
2017-02-17 17:07:40 +0100

    io

---
+++ confirmé/io/io.go
@@ -0,0 +1,46 @@
+package main
+
+import(
+	"fmt"
+	"io/ioutil"
+	"os"
+	"bufio"
+)
+func main() {
+	/* Lecture vec ioutil */
+	content, err := ioutil.ReadFile("io.txt")
+	if err != nil {
+		fmt.Println("erreur à la lecture du fichier", err)
+	}
+	fmt.Println(string(content))
+
+	/* Lecture avec os et bufio */
+	file, err := os.Open("io.txt")
+	if err != nil {
+		fmt.Println("erreur à l'ouverture du fichier", err)
+	}
+	fileStats, err := file.Stat()
+	if err != nil {
+		fmt.Println("erreur à la récupération des informations sur le fichier", err)
+	}
+	reader := bufio.NewReader(file)
+	fileContentBytes, err := reader.Peek(int(fileStats.Size()))
+	if err != nil {
+		fmt.Println("erreur à la lecture du contenu du fichier", err)
+	}
+	fmt.Println(string(fileContentBytes))
+	file.Close()
+
+	/* Ecriture avec ioutil */
+
+	/* Ecriture avec os */
+	newFile, err := os.Create("iobis.txt")
+	if err != nil {
+		fmt.Println("erreur à la création du fichier", err)
+	}
+	_, err = newFile.Write([]byte("helloworld"))
+	if err != nil {
+		fmt.Println("erreur à l'écriture dans le fichier", err)
+	}
+	/* Ecriture avec bufio */
+}
